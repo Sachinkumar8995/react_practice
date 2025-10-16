ed7f83f0071431a80a28457cc2582a670871b352
sachin kumar <sachinkumarmth845401@gmail.com>
2025-10-16 19:32:37 +0530

    01-folder done

---
+++ first-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1 class="font-bold text-6xl">Our first React project</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
